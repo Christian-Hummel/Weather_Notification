cce4e2eabad4f90f6f6351fc7a1dec8a62438d0c
Matthias Hummel <matthiashummel@ymail.com>
2022-10-09 20:06:54 +0200

    define rain state parsing task

---
+++ lib/parser.py
@@ -0,0 +1,12 @@
+
+def parse_current_rain_state(path: str) -> str:
+    """Open a csv file containing weather data and parse the current rain state.
+
+    Args:
+        path: Path to the csv file.
+
+    Returns:
+        Rain state as a string.
+
+    """
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+
+from lib import parser
+
+
+WEATHER_CSV_PATH = "data/weather.csv"
+
+
+def run_weather_notification():
+    rain_state = parser.parse_current_rain_state(WEATHER_CSV_PATH)
+    print(rain_state)
+
+
+if __name__ == "__main__":
+    run_weather_notification()
