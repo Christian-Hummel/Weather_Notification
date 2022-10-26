610d659f61ce7ac864fca10b2e0f86ebd4700eae
Christian-Hummel <chrishummel@gmx.at>
2022-10-26 18:51:24 +0200

    rearranged path variable and open function

--- lib/parser.py
+++ lib/parser.py
@@ -9,13 +9,12 @@
 
     """
 import csv
-def parse_current_rain_state(WEATHER_CSV_PATH: str) -> str:
-    path = WEATHER_CSV_PATH
+def parse_current_rain_state(path: str) -> str:
+
 
 
     with open(path, "r") as f:
         reader = csv.reader(f, delimiter=",")
-        header = next(reader)
-        for row in reader:
-            preciptype = row[13]
-            return preciptype
+        header = next(reader) and next(reader)
+        preciptype = header[13]
+        return preciptype
