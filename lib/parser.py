45b79908c78222fb1752e858b72be3083ece42d8
Christian-Hummel <chrishummel@gmx.at>
2022-10-26 19:31:40 +0200

    minor changes in open function

--- lib/parser.py
+++ lib/parser.py
@@ -15,6 +15,7 @@ def parse_current_rain_state(path: str) -> str:
 
     with open(path, "r") as f:
         reader = csv.reader(f, delimiter=",")
-        header = next(reader) and next(reader)
-        preciptype = header[13]
+        header = next(reader)
+        first_line = next(reader)
+        preciptype = first_line[13]
         return preciptype
