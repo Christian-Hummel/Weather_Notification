a5e1e3ec73bce953aa178708b78512c1c974ace3
Matthias Hummel <matthiashummel@ymail.com>
2022-11-07 21:33:11 +0100

    change path argument to json file

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from lib import parser
 
 
 WEATHER_CSV_PATH = "data/weather.csv"
+WEATHER_JSON_PATH = "data/weather.json"
 
 
 def run_weather_notification():
-    rain_state = parser.parse_json_rain_state(WEATHER_CSV_PATH)
+    rain_state = parser.parse_json_rain_state(WEATHER_JSON_PATH)
     print(rain_state)
 
     notification_doc = open("data/TODO.txt", "w")
