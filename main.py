6e861eb8f2f2ecfdbc5aa6cdc7c1520a2c45e3af
Christian-Hummel <chrishummel@gmx.at>
2022-11-07 18:55:02 +0100

    write preciptype into .txt file

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ WEATHER_CSV_PATH = "data/weather.csv"
 
 
 
-
 def run_weather_notification():
     rain_state = parser.parse_current_rain_state(WEATHER_CSV_PATH)
     print(rain_state)
+
+    notification_doc = open("data/TODO.txt", "w")
+    notification_doc.write(rain_state)
+    notification_doc.close()
 
 
 
