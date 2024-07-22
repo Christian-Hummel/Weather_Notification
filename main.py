4483658d1b31385ac09bf38a40972cf79fda1f6c
Christian-Hummel <chrishummel@gmx.at>
2024-07-22 17:17:30 +0200

    synchronize main file with main file of secondary branch

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ WEATHER_CSV_PATH = "data/weather.csv"
 WEATHER_JSON_PATH = "data/weather.json"
 
 
+
+
 def run_weather_notification():
     # rain_state = parser.parse_json_rain_state(WEATHER_JSON_PATH)
-    weather_data = query.query_current_weather_data("Krems")
-    print(weather_data)
-    print("weather")
-    print("received data:", weather_data)
+    weather_data = query.query_current_weather_data('location')
+    notification = parser.get_current_day_notification(weather_data)
+    print(notification)
+
 
 
 if __name__ == "__main__":
