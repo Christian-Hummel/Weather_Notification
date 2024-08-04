674b37e0cfac3662c6bf8c4d3c6a5fff7e542cf9
Christian-Hummel <chrishummel@gmx.at>
2024-08-04 23:51:44 +0200

    connected mail parse functions with main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from lib import parser
 from lib import query
+from lib import messageparse
 
 WEATHER_CSV_PATH = "data/weather.csv"
 WEATHER_JSON_PATH = "data/weather.json"
@@ -13,6 +14,10 @@ def run_weather_notification():
     weather_data = query.query_current_weather_data('location')
     notification = parser.get_current_day_notification(weather_data)
     print(notification)
+    print(weather_data)
+    message = messageparse.fetch_message()
+    print(message)
+    #messageparse.save_to_file(message)
 
 
 
